9659ae4976608533cedc0c3f9a9378ad082a15f6
Ben <bberrabah@yahoo.fr>
2019-12-18 21:42:36 +0100

    mplementataion du Service

---
+++ hellocucumber/src/test/java/hellocucumber/service/AdresseServiceImpl.java
@@ -0,0 +1,16 @@
+package hellocucumber.service;
+
+import org.springframework.stereotype.Service;
+
+import net.thucydides.core.annotations.NotImplementedException;
+
+@Service
+public class AdresseServiceImpl implements AdresseService{
+
+	@Override
+	public void ModifierAdresse(String aCanal, Boolean aActive, String aPays, Boolean aWithDateEffet) {
+		// TODO Auto-generated method stub
+		throw new NotImplementedException("En cours d'implémentation");
+	}
+
+}
